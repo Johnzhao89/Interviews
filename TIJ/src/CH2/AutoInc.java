33104382b5b40d39359efab0756a75fd9a69cda0
hyuna915 <heguang.liu915@gmail.com>
2013-07-25 18:43:07 -0700

    Ch2

---
+++ TIJ/src/CH2/AutoInc.java
@@ -0,0 +1,14 @@
+package CH2;
+
+public class AutoInc {
+    public static void main(String[] args) {
+      int i = 1;
+      System.out.print("i : " + i);
+      System.out.print("++i : " + ++i); // Pre-increment
+      System.out.print("i++ : " + i++); // Post-increment
+      System.out.print("i : " + i);
+      System.out.print("--i : " + --i); // Pre-decrement
+      System.out.print("i-- : " + i--); // Post-decrement
+      System.out.print("i : " + i);
+}
+}
