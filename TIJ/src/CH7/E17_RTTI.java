2d12424824c28fb5532e453196d75a6670057990
hyuna915 <heguang.liu915@gmail.com>
2013-08-03 16:12:39 -0700

    Ch7 done

--- TIJ/src/CH7/E17_RTTI.java
+++ TIJ/src/CH7/E17_RTTI.java
@@ -9,3 +9,30 @@ package CH7;
  * observe the results. Downcast and call
  * balance() and observe what happens.
  ***********************************************/
+class Useful {
+	  public void f() {}
+	  public void g() {}
+	}
+	class MoreUseful extends Useful {
+	  public void f() {}
+	  public void g() {}
+	  public void u() {}
+	  public void v() {}
+	  public void w() {}
+	}
+	public class E17_RTTI {
+	  public static void main(String[] args) {
+	    Useful[] x = {
+	      new Useful(),
+	      new MoreUseful()
+	    };
+	    x[0].f();
+	    x[1].g();
+	    // Compile time: method not found in Useful:
+	    //! x[1].u();
+	    ((MoreUseful)x[1]).u(); // Downcast/RTTI
+	    ((MoreUseful)x[0]).u(); // Exception thrown
+	}
+	}
+
+
