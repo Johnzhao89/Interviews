2cdc5fbfae38e283ddd278311cbabae0b01782bf
hyuna915 <heguang.liu915@gmail.com>
2013-08-07 16:38:10 -0700

    LinkedList

--- TIJ/src/CH10/E14_MiddleInsertion.java
+++ TIJ/src/CH10/E14_MiddleInsertion.java
@@ -4,3 +4,18 @@ package CH10;
  * ListIterator, add Integers to the List by always
  * inserting them in the middle of the List.
  ***********************************************/
+
+import java.util.*;
+
+public class E14_MiddleInsertion{
+	public static void main(String[] args){
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		ListIterator<Integer> it = list.listIterator();
+		for(int i=1; i<=10; i++){
+			it.add(i);
+			if( i% 2 == 0)
+				it.previous();
+		}
+		System.out.println(list);
+	}
+}
